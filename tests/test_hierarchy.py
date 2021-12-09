a5fe6bc50519d19a82a8f9fef870fa87711867b9
bill-bateman <bill@batemanzhou.com>
2021-12-08 16:29:54 -0800

    Adding slightly more complicated hierarchy example.

--- tests/test_hierarchy.py
+++ tests/test_hierarchy.py
@@ -18,3 +18,15 @@ def test_basic():
     assert root['pos'][0][2]==pytest.approx(0.2)
     assert root['pos'][1][2]==pytest.approx(0.4)
     assert root['pos'][2][2]==pytest.approx(0.6)
+
+def test_nested():
+    root = {
+        'children': [
+            {'r': 1, 'children': [{'r':1,'r':2}]},
+            {'r': 2},
+            {'r': 3},
+        ]
+    }
+    pack_enclose_hierarchy(root)
+    assert 'r' in root
+    assert root['r']==1
